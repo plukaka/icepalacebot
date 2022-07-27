f761b78696c5192d63b0817da9cc4f49ff6755c5
Paul Lukaka <plukaka@yandex.kg>
2022-07-27 09:46:21 +0300

    audio

--- icepalace_bot.py
+++ icepalace_bot.py
@@ -7,10 +7,12 @@ from aiogram.utils import executor
 
 import config
 
-import os, json, string
+import os, json, string, uuid
+import speech_recognition as sr
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
+r = sr.Recognizer()
 
 async def on_startup(dp):
 	await bot.set_webhook(config.URL_APP)
@@ -18,6 +20,39 @@ async def on_startup(dp):
 async def on_shutdown(dp):
 	await bot.delete_webhook()
 
+'''******** АУДИО **********'''
+
+async def recognise(filename):
+    with sr.AudioFile(filename) as source:
+        audio_text = r.listen(source)
+        try:
+            text = r.recognize_google(audio_text,language=language)
+            print('Converting audio transcripts into text ...')
+            print(text)
+            return text
+        except:
+            print('Sorry.. run again...')
+            return "Sorry.. run again..."
+
+@dp.message_handler(content_types=['voice'])
+async def voice_processing(message):
+	 filename = str(uuid.uuid4())
+    file_name_full="./voice/"+filename+".ogg"
+    file_name_full_converted="./ready/"+filename+".wav"
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
+    text=recognise(file_name_full_converted)
+    bot.reply_to(message, text)
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
+
+
+@dp.message_handler(content_types=['new_chat_members'])
+async def new_chat(message : types.Message):
+	await message.answer('Приветствуем! Ты стал членом нашего дружного вратарского чата! Здесь ты сможешь обсудить, поболтать и поныть. Но учти оскорбления и мат запрещены!')
 
 
 #функция для показа в консоли сообщения, пишем тут и добавляем
@@ -43,6 +78,7 @@ async def echo_send(message : types.Message):
 		.intersection(set(json.load(open('cenz.json')))) != set():
 		await message.reply('Мат запрещен!')
 		#await message.delete()
+
 
 #Приветствие нового участника группы
 @dp.message_handler(content_types=['new_chat_members'])
