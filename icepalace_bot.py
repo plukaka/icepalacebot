3b833f7b0957dbc3e174918a61be07f2ea8bf63f
Paul Lukaka <plukaka@yandex.kg>
2022-07-27 09:50:53 +0300

    audio2

--- icepalace_bot.py
+++ icepalace_bot.py
@@ -36,18 +36,18 @@ async def recognise(filename):
 
 @dp.message_handler(content_types=['voice'])
 async def voice_processing(message):
-	 filename = str(uuid.uuid4())
-    file_name_full="./voice/"+filename+".ogg"
-    file_name_full_converted="./ready/"+filename+".wav"
-    file_info = bot.get_file(message.voice.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open(file_name_full, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
-    text=recognise(file_name_full_converted)
-    bot.reply_to(message, text)
-    os.remove(file_name_full)
-    os.remove(file_name_full_converted)
+	filename = str(uuid.uuid4())
+	file_name_full="./voice/"+filename+".ogg"
+	file_name_full_converted="./ready/"+filename+".wav"
+	file_info = bot.get_file(message.voice.file_id)
+	downloaded_file = bot.download_file(file_info.file_path)
+	with open(file_name_full, 'wb') as new_file:
+		new_file.write(downloaded_file)
+	os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
+	text=recognise(file_name_full_converted)
+	bot.reply_to(message, text)
+	os.remove(file_name_full)
+	os.remove(file_name_full_converted)
 
 
 @dp.message_handler(content_types=['new_chat_members'])
