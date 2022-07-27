590dfe29a35d67fabb88d044ce55968bdf7bb4bd
Paul Lukaka <plukaka@yandex.kg>
2022-07-27 10:27:27 +0300

    new audio

--- icepalace_bot.py
+++ icepalace_bot.py
@@ -9,10 +9,15 @@ import config
 
 import os, json, string, uuid
 import speech_recognition as sr
+import soundfile as sf
+import requests
+import subprocess
+import datetime
 
+logfile = str(datetime.date.today()) + '.log'
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
-r = sr.Recognizer()
+
 
 async def on_startup(dp):
 	await bot.set_webhook(config.URL_APP)
@@ -21,33 +26,69 @@ async def on_shutdown(dp):
 	await bot.delete_webhook()
 
 '''******** АУДИО **********'''
-
-async def recognise(filename):
-    with sr.AudioFile(filename) as source:
-        audio_text = r.listen(source)
-        try:
-            text = r.recognize_google(audio_text,language=language)
-            print('Converting audio transcripts into text ...')
-            print(text)
-            return text
-        except:
-            print('Sorry.. run again...')
-            return "Sorry.. run again..."
+async def audio_to_text(dest_name: str):
+# Функция для перевода аудио , в формате ".vaw" в текст
+	r = sr.Recognizer()
+# тут мы читаем наш .vaw файл
+	message = sr.AudioFile(dest_name)
+	with message as source:
+		audio = r.record(source)
+	result = r.recognize_google(audio, language="ru_RU") # здесь можно изменять язык распознавания
+	return result
 
 @dp.message_handler(content_types=['voice'])
-async def voice_processing(message):
-	filename = str(uuid.uuid4())
-	file_name_full="./voice/"+filename+".ogg"
-	file_name_full_converted="./ready/"+filename+".wav"
-	file_info = bot.get_file(message.voice.file_id)
-	downloaded_file = bot.download_file(file_info.file_path)
-	with open(file_name_full, 'wb') as new_file:
-		new_file.write(downloaded_file)
-	os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
-	text=recognise(file_name_full_converted)
-	bot.reply_to(message, text)
-	os.remove(file_name_full)
-	os.remove(file_name_full_converted)
+async def get_audio_messages(message):
+# Основная функция, принимает голосовуху от пользователя
+	try:
+		print("Started recognition...")
+# Ниже пытаемся вычленить имя файла, да и вообще берем данные с мессаги
+		file_info = bot.get_file(message.voice.file_id)
+		path = file_info.file_path # Вот тут-то и полный путь до файла (например: voice/file_2.oga)
+		fname = os.path.basename(path) # Преобразуем путь в имя файла (например: file_2.oga)
+		doc = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))# Получаем и сохраняем присланную голосвуху (Ага, админ может в любой момент отключить удаление айдио файлов и слушать все, что ты там говоришь. А представь, что такую бяку подселят в огромный чат и она будет просто логировать все сообщения [анонимность в телеграмме, ахахаха])
+		with open(fname+'.oga', 'wb') as f:
+			f.write(doc.content) # вот именно тут и сохраняется сама аудио-мессага
+		process = subprocess.run(['ffmpeg', '-i', fname+'.oga', fname+'.wav'])# здесь используется страшное ПО ffmpeg, для конвертации .oga в .vaw
+		result = audio_to_text(fname+'.wav') # Вызов функции для перевода аудио в текст, а заодно передаем имена файлов, для их последующего удаления
+		bot.send_message(message.from_user.id, format(result))
+	except sr.UnknownValueError as e:
+		bot.send_message(message.from_user.id,  "Прошу прощения, но я не разобрал сообщение, или оно поустое...")
+		with open(logfile, 'a', encoding='utf-8') as f:
+			f.write(str(datetime.datetime.today().strftime("%H:%M:%S")) + ':' + str(message.from_user.id) + ':' + str(message.from_user.first_name) + '_' + str(message.from_user.last_name) + ':' + str(message.from_user.username) +':'+ str(message.from_user.language_code) + ':Message is empty.\n')
+	except Exception as e:
+		bot.send_message(message.from_user.id,  "Что-то пошло через жопу, но наши смелые инженеры уже трудятся над решением... \nДа ладно, никто эту ошибку исправлять не будет, она просто потеряется в логах.")
+		with open(logfile, 'a', encoding='utf-8') as f:
+			f.write(str(datetime.datetime.today().strftime("%H:%M:%S")) + ':' + str(message.from_user.id) + ':' + str(message.from_user.first_name) + '_' + str(message.from_user.last_name) + ':' + str(message.from_user.username) +':'+ str(message.from_user.language_code) +':' + str(e) + '\n')
+	finally:
+		os.remove(fname+'.wav')
+		os.remove(fname+'.oga')
+
+# async def recognise(filename):
+#     with sr.AudioFile(filename) as source:
+#         audio_text = r.listen(source)
+#         try:
+#             text = r.recognize_google(audio_text,language=language)
+#             print('Converting audio transcripts into text ...')
+#             print(text)
+#             return text
+#         except:
+#             print('Sorry.. run again...')
+#             return "Sorry.. run again..."
+
+# @dp.message_handler(content_types=['voice'])
+# async def voice_processing(message):
+# 	filename = str(uuid.uuid4())
+# 	file_name_full="./voice/"+filename+".ogg"
+# 	file_name_full_converted="./ready/"+filename+".wav"
+# 	file_info = bot.get_file(message.voice.file_id)
+# 	downloaded_file = bot.download_file(file_info.file_path)
+# 	with open(file_name_full, 'wb') as new_file:
+# 		new_file.write(downloaded_file)
+# 	os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
+# 	text=recognise(file_name_full_converted)
+# 	bot.reply_to(message, text)
+# 	os.remove(file_name_full)
+# 	os.remove(file_name_full_converted)
 
 
 @dp.message_handler(content_types=['new_chat_members'])
